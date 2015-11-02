9164354f13b3d42539eaaefe79b82996a0df0465
moxpower <moxpower@users.noreply.github.com>
2015-11-02 12:54:25 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-consumer_key = '7m6bo2eEvpvGzFDH2n9miaBrg'
-consumer_secret = 'e1Xm3GDbhUXjGHXMDEzX5PZTGBpbaNRg2Apa6DLnp9Vd7FiejU'
-access_token = '582691032-p0iYWS3bPxwJSNivmDpxKIrpegR5QaTZnWvsfqXR'
-access_secret = '3Az4zIfUriNQJIpz4uE6VOx2gsuqy64UtEXNYZrOk8BTB'
+consumer_key = '###'
+consumer_secret = '###'
+access_token = '###'
+access_secret = '###'
