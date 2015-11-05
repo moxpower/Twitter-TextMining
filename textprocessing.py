6aed9b4b4190882a5afcd3728da5ec0693c2ff8e
moxpower <maxkohl@gmx.de>
2015-11-05 19:24:29 +0100

    termfreq

--- textprocessing.py
+++ textprocessing.py
@@ -4,12 +4,14 @@ from collections import Counter
 from nltk.corpus import stopwords
 import string
 import pandas as pd
+import vincent
 
 punctuation = list(string.punctuation)
 stop = stopwords.words('english') + punctuation + stopwords.words('german') + ['rt','via',':','RT',u'\u2026', u'\ud83d', u'\xfcck']
 
 fname='twitter_stream.4.json'
 oname='data.tsv'
+visname='term_freq.json'
  
 emoticons_str = r"""
     (?:
@@ -54,10 +56,14 @@ with open(fname,'r') as f:
         terms_all = [term.encode('utf-8') for term in preprocess(tweet['text']) if term not in stop]
         count_all.update(terms_all)
         countres = count_all.most_common(30)
-        df=pd.DataFrame(countres)
+        """df=pd.DataFrame(countres)
         df.columns=['token','quantity']
         df=df.set_index(['token'])
-        df.to_csv(oname,sep = ',')
+        df.to_csv(oname,sep = '\t')"""
+        labels, freq = zip(*countres)
+        data = {'data':freq, 'x': labels}
+        bar = vincent.Bar(data, iter_idx='x')
+        bar.to_json(visname)
             
     print(count_all.most_common(30))
 #        tweettext = preprocess(tweet['text'].encode('utf-8'))
